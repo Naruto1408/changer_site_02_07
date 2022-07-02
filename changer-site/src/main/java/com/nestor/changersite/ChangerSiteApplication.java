127f85e346b711ccf6d4178ff5427f7eee59c0e8
Naruto1408 <nestor.zasymenko@gmail.com>
2022-07-02 20:40:36 +0200

    first commit

---
+++ changer-site/src/main/java/com/nestor/changersite/ChangerSiteApplication.java
@@ -0,0 +1,13 @@
+package com.nestor.changersite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChangerSiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChangerSiteApplication.class, args);
+	}
+
+}
---
+++ changer-site/src/test/java/com/nestor/changersite/ChangerSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nestor.changersite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChangerSiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
